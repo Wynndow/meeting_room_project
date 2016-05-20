fe5d2d070d144b0c4c8c18c0a8d16c4b9e924ccd
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2016-05-20 13:53:41 +0100

    Add first test of root
    Checks status code and Hello World

---
+++ tests/test_client.py
@@ -0,0 +1,13 @@
+import pytest
+from app import create_app
+from flask import url_for
+
+class TestCase():
+    def setup(self):
+        self.app = create_app('testing')
+        self.client = self.app.test_client()
+
+    def test_home_page(self):
+        response = self.client.get('/')
+        assert response.status_code == 200
+        assert ('Hello world 2' in response.get_data(as_text=True))
