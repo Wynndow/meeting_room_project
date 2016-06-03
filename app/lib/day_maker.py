8a89dc14fb2fd0d031c9f7a395e50127d2cd494c
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2016-06-03 15:25:02 +0100

    Refactor

--- app/lib/day_maker.py
+++ app/lib/day_maker.py
@@ -8,14 +8,14 @@ def create_full_day_json(data):
         if len(output) == 0:
             length = _time_in_minutes(booking.get('start'))
             times = '00:00 to {}'.format(booking.get('start')[-9:-4])
-            output.append({'times': times, 'length': length, 'status': 'Free'})
+            _add_block_to_day(output, times, length, 'Free')
 
             start = length
             end = _time_in_minutes(booking.get('end'))
             length = end - start
             times = '{} to {}'.format(booking.get('start')[-9:-4], booking.get('end')[-9:-4])
 
-            output.append({'times': times, 'length': length, 'status': 'Busy'})
+            _add_block_to_day(output, times, length, 'Busy')
 
             continue
 
@@ -24,24 +24,26 @@ def create_full_day_json(data):
             length = _time_in_minutes(booking.get('end')) - _time_in_minutes(booking.get('start'))
             times = '{} to {}'.format(booking.get('start')[-9:-4], booking.get('end')[-9:-4])
 
-            output.append({'times': times, 'length': length, 'status': 'Busy'})
+            _add_block_to_day(output, times, length, 'Busy')
             continue
 
         times = '{} to {}'.format(output[-1].get('times')[-5:], booking.get('start')[-9:-4])
-        output.append({'times': times, 'length': length, 'status': 'Free'})
+        _add_block_to_day(output, times, length, 'Free')
 
         length = _time_in_minutes(booking.get('end')) - _time_in_minutes(booking.get('start'))
         times = '{} to {}'.format(booking.get('start')[-9:-4], booking.get('end')[-9:-4])
 
-        output.append({'times': times, 'length': length, 'status': 'Busy'})
+        _add_block_to_day(output, times, length, 'Busy')
 
     if not output[-1].get('times')[-5:] == '00:00' and not len(data) == 0:
         length = 1440 - _time_in_minutes(output[-1].get('times')[-5:])
         times = '{} to {}'.format(output[-1].get('times')[-5:], '00:00')
-        output.append({'times': times, 'length': length, 'status': 'Free'})
+        _add_block_to_day(output, times, length, 'Free')
 
-    print json.dumps(output, sort_keys=True, indent=4)
     return output
+
+def _add_block_to_day(output, times, length, status):
+    output.append({'times': times, 'length': length, 'status': status})
 
 
 def _time_in_minutes(time_string):
