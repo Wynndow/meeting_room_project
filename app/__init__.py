08dccac98c1ea9e642376da84a7ee82c99485502
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2016-06-17 16:25:15 +0100

    Register bootstrap with the app

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from flask.ext.bootstrap import Bootstrap
 from config import config
 
+bootstrap = Bootstrap()
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+
+    bootstrap.init_app(app)
 
     from main import main as main_blueprint
     app.register_blueprint(main_blueprint)
