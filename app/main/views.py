178ff4cb235b6cdc75acf3d3322d33d651104488
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2016-06-17 15:14:20 +0100

    Add times to view
    Leading zero's added for time column

--- app/main/views.py
+++ app/main/views.py
@@ -1,15 +1,16 @@
 from flask import render_template, request
 from . import main
-from ..lib import api_client, day_maker
+from ..lib import api_client, day_maker, number_padder
 from datetime import datetime
 
 
 @main.route('/')
 def index():
     floor = request.args.get('floor', 'third')
-    date = request.args.get('date', str(datetime.utcnow())[0:10]) 
+    date = request.args.get('date', str(datetime.utcnow())[0:10])
     rooms = api_client.get_room_list(floor)
     free_busy = api_client.get_free_busy(rooms, date).get('calendars')
     full_days = day_maker.create_full_days(rooms, free_busy)
+    times = number_padder.add_zero(24)
 
-    return render_template('index.html', rooms=rooms, full_days=full_days)
+    return render_template('index.html', rooms=rooms, full_days=full_days, times=times)
