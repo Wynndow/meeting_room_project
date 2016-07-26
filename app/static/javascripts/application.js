886f853ca2cd37e9e6b548461f96354cfc142009
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2016-07-26 13:46:14 +0100

    Show selected blocks time and status in header

--- app/static/javascripts/application.js
+++ app/static/javascripts/application.js
@@ -1,3 +1,15 @@
 $( document ).ready(function(){
+
   $('body').scrollTop(475);
+
+  $('.Free, .Busy').hover(
+    function() {
+      $('#header-status').text($( this ).find( 'div:first' ).text());
+      $('#header-time').text($( this ).find( 'div:last' ).text())
+    }, function() {
+      $('#header-status').text('');
+      $('#header-time').text('');
+    }
+  )
+
 })
