c4acb43c23c95b22b6936acfef671c5bfe8b1080
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2016-08-15 15:05:20 +0100

    Make pep8 happy

--- app/exceptions.py
+++ app/exceptions.py
@@ -4,5 +4,5 @@ class InvalidUsage(Exception):
     def __init__(self, message, status_code=None):
         Exception.__init__(self)
         self.message = message
-        if status_code is not None :
+        if status_code is not None:
             self.status_code = status_code
--- app/main/errors.py
+++ app/main/errors.py
@@ -2,6 +2,7 @@ from flask import render_template
 from . import main
 from ..exceptions import InvalidUsage
 
+
 @main.app_errorhandler(InvalidUsage)
 def handle_invalid_usage(e):
     return render_template('invalid.html', message=e.message), 400
