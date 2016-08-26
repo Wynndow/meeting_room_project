1327e7fc1a1acb364a348fc68b434b8cdee03cc3
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2016-08-26 11:05:17 +0100

    Import RoomList and extract logic

--- app/lib/api_client.py
+++ app/lib/api_client.py
@@ -2,50 +2,13 @@ import json
 from datetime import datetime
 from ..exceptions import InvalidUsage
 from flask import current_app
+from ..lib.room_list import RoomList
 
 
 def get_room_list(floor):
     resources = _fetch_resources()
     filtered = _filter_rooms(resources, floor)
-    return _neaten_room_names(filtered)
-
-
-def get_free_busy(room_list, date):
-    times = _set_times(date)
-    calendar_ids = _extract_calendar_ids(room_list)
-    body = _build_free_busy_body(times, calendar_ids)
-    calendar = current_app.config['CALENDAR']
-    return calendar.freebusy().query(body=body).execute()
-
-
-def _set_times(date):
-    try:
-        today = datetime.strptime(date, '%Y-%m-%d')
-    except ValueError:
-        raise InvalidUsage("You've submitted an invalid date!")
-
-    start = today.replace(hour=0, minute=0, second=0).isoformat() + 'Z'
-    end = today.replace(hour=23, minute=59, second=59).isoformat() + 'Z'
-    return {
-        'start': start,
-        'end': end
-    }
-
-
-def _build_free_busy_body(times, calendar_ids):
-    return {
-        "timeMin": times['start'],
-        "timeMax": times['end'],
-        "timeZone": 'GMT+01:00',
-        "items": calendar_ids
-    }
-
-
-def _extract_calendar_ids(room_list):
-    calendars = []
-    for room in room_list:
-        calendars.append({"id": room.get('resourceEmail')})
-    return calendars
+    return RoomList(_neaten_room_names(filtered))
 
 
 def _fetch_resources():
