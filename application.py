5714a4932456220afdfe00c69b702b34d081a8bf
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2016-11-07 11:43:58 +0000

    Schedule emails to go at 7.30am

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from app.lib.email_trigger import EmailTrigger
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(EmailTrigger.call, 'cron', hour=8, minute=30, timezone='Europe/London')
+scheduler.add_job(EmailTrigger.call, 'cron', hour=7, minute=30, timezone='Europe/London')
 scheduler.start()
 
 application = create_app(os.getenv('FLASK_CONFIG') or 'default')
