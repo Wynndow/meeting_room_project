82d57030e107c4ac9a81df51e7716c119dc3849b
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2018-08-29 10:39:55 +0100

    Bump Flask to 1.0.2

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from app import create_app
 import os
-from flask.ext.script import Manager
+from flask_script import Manager
 
 application = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(application)
